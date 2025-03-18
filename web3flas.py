056e14d105f1c7e4de0ce6868725ba1f976ac444
tojonoy <thomasjohn3933@gmail.com>
2025-03-18 15:49:17 +0530

    V3.0 changes

--- web3flas.py
+++ web3flas.py
@@ -346,5 +346,5 @@ def authenticate_user():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set
-    app.run(host="0.0.0.0", port=port,debug=True)
+    app.run(host="0.0.0.0", port=port)
     #app.run(debug=True)
